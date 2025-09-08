9009b052d6c2655b373559e3a28901f7d898b202
Igor Anić <igor.anic@gmail.com>
2025-09-08 11:47:30 +0200

    add benchmark script

--- ab.sh
+++ ab.sh
@@ -1,22 +1,39 @@
 #!/bin/bash -e
 
+targets() {
+    protocol=$1
+    port=$2
+    cd ~/Code/www.ziglang.org/zig-out
+    find . -type f -exec echo -e "GET $protocol://localhost:$port/{}\n" \; >~/Code/httpz/targets
+    cd - >>/dev/null
+}
+
 cd ~/Code/www.ziglang.org/zig-out
 echo files count: $(find . -type f | wc -l)
-echo vegeta httpz
-find . -type f -exec echo -e "GET http://localhost:8080/{}\n" \; >~/Code/httpz/targets
 cd - >>/dev/null
+
+echo https httpz
+targets https 8443
 vegeta attack -targets=targets -duration=10s -rate=0 -max-workers=1024 | vegeta report
 
-echo nginx
-cd ~/Code/www.ziglang.org/zig-out
-find . -type f -exec echo -e "GET http://localhost:8081/{}\n" \; >~/Code/httpz/targets
-cd - >>/dev/null
+echo https nginx
+targets https 8444
+vegeta attack -targets=targets -duration=10s -rate=0 -max-workers=1024 | vegeta report
+
+echo
+echo http httpz
+targets http 8080
+vegeta attack -targets=targets -duration=10s -rate=0 -max-workers=1024 | vegeta report
+
+echo http nginx
+targets http 8081
 vegeta attack -targets=targets -duration=10s -rate=0 -max-workers=1024 | vegeta report
 
 exit
 
-ab -n 50000 -c 1 http://localhost:8080/index.html 2>&1 | grep "Requests per second:"
-ab -n 50000 -c 1 http://localhost:8081/index.html 2>&1 | grep "Requests per second:"
+# notes
+ab -n 100 -c 1 https://localhost:8443/index.html 2>&1 | grep "Requests per second:"
+ab -n 100 -c 1 http://localhost:8081/index.html 2>&1 | grep "Requests per second:"
 
 exit
 
